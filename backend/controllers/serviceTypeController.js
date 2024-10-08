00b340d5f5da6851a79a93a9f97faeee58f1d713
Ragapiriya <r2001piriya@gmail.com>
2024-10-09 00:15:44 +0530

    minor changes made

---
+++ backend/controllers/serviceTypeController.js
@@ -0,0 +1,17 @@
+const serviceTypeModel = require('../models/serviceTypeModel');
+const ErrorHandler = require('../utils/errorHandler');
+const catchAsyncError = require("../middlewares/catchAsyncError");
+
+
+//create service type- ADMIN
+//delete service type - ADMIN
+//modify service type -ADMIN
+
+//get all service type 
+exports.getServiceTypes = catchAsyncError( async (req, res, next) => {
+    const serviceTypes = serviceTypeModel.find();
+    res.status(200).json({
+        success:true,
+        serviceTypes,
+    })
+});
---
+++ backend/server.js
@@ -0,0 +1,20 @@
+const app = require('./app');
+const dotenv = require('dotenv');
+const pathModule = require('path');
+const connectDatabase = require('./config/database');
+
+dotenv.config({path:pathModule.join(__dirname,"config/config.env")});
+connectDatabase();
+const server = app.listen(process.env.PORT,()=>{console.log(`Server listening to the port ${process.env.PORT} in ${process.env.NODE_ENV}`)});
+
+
+//incase of uncaught errors
+process.on('uncaughtException',(err)=>{
+    console.log(`Error: ${err.message}`);
+    console.log("Shutting down the server due to uncaught exception error.")
+    server.close(()=>{//to stop server 
+        process.exit(1); //after stopping the server node program will come to end
+    }); 
+
+})
+
