c801a412413af14698ccb7d94d5c4ff4a01475af
Ragapiriya <r2001piriya@gmail.com>
2024-10-09 22:37:47 +0530

    db connection

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ connectDatabase();
 const server = app.listen(process.env.PORT,()=>{console.log(`Server listening to the port ${process.env.PORT} in ${process.env.NODE_ENV}`)});
 
 
-//incase of uncaught errors
+//incase of uncaught errors 
 process.on('uncaughtException',(err)=>{
     console.log(`Error: ${err.message}`);
     console.log("Shutting down the server due to uncaught exception error.")
